6f92886667ed500d8f6272c56d2737fc7d5452df
ObsidianLua <lifedupey@gmail.com>
2026-09-10 00:27:46 +1000

    Update Fisher Hub.lua

--- Fisher Hub.lua
+++ Fisher Hub.lua
@@ -1,3 +1,12 @@
+--// Shared access check
+local accessOk, allowedUserIds = pcall(function()
+	return loadstring(game:HttpGet("https://raw.githubusercontent.com/ObsidianLua/fisher-scripts/main/AccessWhitelist.lua"))()
+end)
+
+if not accessOk or type(allowedUserIds) ~= "table" or not allowedUserIds[game:GetService("Players").LocalPlayer.UserId] then
+	return
+end
+
 --// Fisher Hub
 --// Credits: @ItzOnlyFisher
 
